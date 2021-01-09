15015396a3e827215d1b1934bb6b23a1bccb099c
mt_9 <hyunsichang@hanyang.ac.kr>
2021-01-09 12:38:21 +0900

    로또번호 분류 코드 생성

--- lotto.R
+++ lotto.R
@@ -1,5 +1,11 @@
+#import
+
 library(httr)
 library(jsonlite)
+library(dplyr)
+library(plyr)
+
+#crawling and write to csv file
 
 basic_url <- 'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo='
 df_lotto_num <- NULL
@@ -15,3 +21,18 @@ for (x in 1:944) {
   print(paste(x , "crawled."))
 }
 write.csv(df_lotto_num, "./lotto.csv")
+
+# manipulate dataframe
+
+df_lotto_num <- read.csv("./lotto.csv")
+df_lotto <- subset(df_lotto_num, select = c("drwtNo1", "drwtNo2", "drwtNo3", "drwtNo4", "drwtNo5", "drwtNo6", "bnusNo"))
+columns <- colnames(df_lotto)
+df_count <- apply(df_lotto, 2, table)
+df_sum <- NULL
+for(item in df_count) {
+  if(is.null(df_sum)) {
+    df_sum <- as.data.frame(df_count$drwtNo1)
+  } else {
+    df_sum <- merge(by=var1)
+  }
+}
