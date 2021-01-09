b44ad1ca00f1ef00df01e473c3fe34ee1146eb09
mt_9 <hyunsichang@hanyang.ac.kr>
2021-01-09 13:09:07 +0900

    로또번호 분류 코드 성공

--- lotto.R
+++ lotto.R
@@ -3,24 +3,24 @@
 library(httr)
 library(jsonlite)
 library(dplyr)
-library(plyr)
+library(data.table)
 
 #crawling and write to csv file
 
-basic_url <- 'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo='
-df_lotto_num <- NULL
-for (x in 1:944) {
-  req_url <- paste0(basic_url, x)
-  df_response <- as.data.frame(fromJSON(req_url))
-  df_sub <- subset(df_response, select = c("drwNo", "drwtNo1", "drwtNo2", "drwtNo3", "drwtNo4", "drwtNo5", "drwtNo6", "bnusNo"))
-  if(x == 1) {
-    df_lotto_num <- df_sub
-  } else {
-      df_lotto_num <- rbind(df_lotto_num, df_sub)
-  }
-  print(paste(x , "crawled."))
-}
-write.csv(df_lotto_num, "./lotto.csv")
+#basic_url <- 'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo='
+#df_lotto_num <- NULL
+#for (x in 1:944) {
+#  req_url <- paste0(basic_url, x)
+#  df_response <- as.data.frame(fromJSON(req_url))
+#  df_sub <- subset(df_response, select = c("drwNo", "drwtNo1", "drwtNo2", "drwtNo3", "drwtNo4", "drwtNo5", "drwtNo6", "bnusNo"))
+#  if(x == 1) {
+#    df_lotto_num <- df_sub
+#  } else {
+#      df_lotto_num <- rbind(df_lotto_num, df_sub)
+#  }
+#  print(paste(x , "crawled."))
+#}
+#write.csv(df_lotto_num, "./lotto.csv")
 
 # manipulate dataframe
 
@@ -33,6 +33,7 @@ for(item in df_count) {
   if(is.null(df_sum)) {
     df_sum <- as.data.frame(df_count$drwtNo1)
   } else {
-    df_sum <- merge(by=var1)
+    df_item <- as.data.frame(item)
+    df_sum = rbindlist(list(df_sum, df_item))[,lapply(.SD, sum, na.rm=TRUE), by = Var1]
   }
 }
