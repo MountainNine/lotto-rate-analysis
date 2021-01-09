4ed640505e77ab206196d2dfea158e72b96b71bf
mt_9 <hyunsichang@hanyang.ac.kr>
2021-01-09 11:04:45 +0900

    로또번호 크롤링 성공

--- lotto.R
+++ lotto.R
@@ -7,7 +7,11 @@ for (x in 1:944) {
   req_url <- paste0(basic_url, x)
   df_response <- as.data.frame(fromJSON(req_url))
   df_sub <- subset(df_response, select = c("drwNo", "drwtNo1", "drwtNo2", "drwtNo3", "drwtNo4", "drwtNo5", "drwtNo6", "bnusNo"))
-  rbind(df_lotto_num, df_sub)
+  if(x == 1) {
+    df_lotto_num <- df_sub
+  } else {
+      df_lotto_num <- rbind(df_lotto_num, df_sub)
+  }
   print(paste(x , "crawled."))
 }
 write.csv(df_lotto_num, "./lotto.csv")
